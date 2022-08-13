59fa4fb82b73572963489dd6e1d106b3566fef1d
mkatanec <matija.katanec@gmail.com>
2022-08-14 00:18:24 +0200

    Short calculating Util #5 Nadogradnja Utila

--- src/test/groovy/net/beardy/finance/shortme/util/ShortUtilSpecification.groovy
+++ src/test/groovy/net/beardy/finance/shortme/util/ShortUtilSpecification.groovy
@@ -40,4 +40,25 @@ class ShortUtilSpecification extends Specification {
     new BigDecimal("24000.00") | new BigDecimal("0.01") || new BigDecimal(24240)
   }
 
+  def "given [sellPrice: #sellPrice, sellAmount: #sellAmount, profit: #profit] should return #result"() {
+    given:
+    def bigDecimalSellPrice = new BigDecimal(sellPrice)
+    def bigDecimalSellAmount = new BigDecimal(sellAmount)
+    def bigDecimalProfit = new BigDecimal(profit)
+
+    when:
+    def response = ShortUtil.calculateShort(bigDecimalSellPrice, bigDecimalSellAmount, bigDecimalProfit)
+
+    then:
+    verifyAll(response) {
+      price == result.price
+      amount == result.amount
+    }
+
+    where:
+    sellPrice | sellAmount | profit || result
+    "24000"   | "0.00202"  | "1"    || ["price": new BigDecimal("23881.77"), "amount": new BigDecimal("0.00203")]
+    "2000"    | "0.015"    | "1"    || ["price": new BigDecimal("1875"), "amount": new BigDecimal("0.016")]
+  }
+
 }
